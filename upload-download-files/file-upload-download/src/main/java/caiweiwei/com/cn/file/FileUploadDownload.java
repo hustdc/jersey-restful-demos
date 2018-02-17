03de4cf4ec80dd88c08e7f31c50946a7cfc10caf
hustdc <dcduanman@163.com>
2018-02-17 13:47:46 +0800

    add post

--- upload-download-files/file-upload-download/src/main/java/caiweiwei/com/cn/file/FileUploadDownload.java
+++ upload-download-files/file-upload-download/src/main/java/caiweiwei/com/cn/file/FileUploadDownload.java
@@ -3,7 +3,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+
 import javax.ws.rs.PathParam;
+import javax.ws.rs.POST;
+
 
 @Path("/files")
 public class FileUploadDownload {
@@ -15,5 +23,27 @@ public class FileUploadDownload {
 	public File getFile(@PathParam("filepath") String path)
 	{
 		return new File(basePath + path);
+	}
+	
+	@POST
+	@Path("{id}")
+	public void post(@PathParam("id") String id, File file) {
+		File newFile = new File(basePath + id);
+		InputStream in;
+		OutputStream out;
+		int tmpByte;
+		try {
+			in = new FileInputStream(file);
+			newFile.createNewFile();
+			out = new FileOutputStream(newFile);
+			while((tmpByte = in.read()) != -1){
+				out.write((byte)tmpByte);
+			}
+			in.close();
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
