e96b234f308ab83fe6a3955dc420c33693b4d129
hustdc <dcduanman@163.com>
2018-02-17 11:12:18 +0800

    jersey download file v1.0

--- upload-download-files/file-upload-download/src/main/java/caiweiwei/com/cn/file/FileUploadDownload.java
+++ upload-download-files/file-upload-download/src/main/java/caiweiwei/com/cn/file/FileUploadDownload.java
@@ -1,6 +1,19 @@
 package caiweiwei.com.cn.file;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import java.io.File;
+import javax.ws.rs.PathParam;
 
-
+@Path("/files")
 public class FileUploadDownload {
+	private static final String basePath = "/home/chao/pictures";
 	
+	@GET
+	@Path("{filepath:.*}")
+	@Produces("text/plain")
+	public File getFile(@PathParam("filepath") String path)
+	{
+		return new File(basePath + path);
+	}
 }
