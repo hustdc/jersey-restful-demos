4f60738570d648bacbfe861a78a03b260fc96faa
hustdc <dcduanman@163.com>
2018-02-18 16:31:43 +0800

    modify produces parameter

--- get-post-json/get-post-json/src/main/java/caiweiwei/com/cn/json/PostGetJson.java
+++ get-post-json/get-post-json/src/main/java/caiweiwei/com/cn/json/PostGetJson.java
@@ -1,13 +1,15 @@
 package caiweiwei.com.cn.json;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Produces;
+
 
 @Path("/json")
 public class PostGetJson {
 	
 	@GET
-	@Produces("application/json")
+	@Produces(MediaType.APPLICATION_JSON)
 	public MyJaxbBean getMyBean() {
 	    return new MyJaxbBean("Agamemnon", 32);
 	}
