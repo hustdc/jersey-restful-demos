4506603262bfa469e7f56234c5ae6a18729f4c4f
hustdc <dcduanman@163.com>
2018-02-18 18:10:32 +0800

    add post json

--- get-post-json/get-post-json/src/main/java/caiweiwei/com/cn/json/PostGetJson.java
+++ get-post-json/get-post-json/src/main/java/caiweiwei/com/cn/json/PostGetJson.java
@@ -3,14 +3,25 @@ import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
 
 
 @Path("/json")
 public class PostGetJson {
 	
+	@Path("/get_json")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public MyJaxbBean getMyBean() {
 	    return new MyJaxbBean("Agamemnon", 32);
 	}
+	
+	@Path("/post_json")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void postMyBean(MyJaxbBean myJaxBean) {
+		System.out.println(myJaxBean.getName());
+	}
+	
 }
